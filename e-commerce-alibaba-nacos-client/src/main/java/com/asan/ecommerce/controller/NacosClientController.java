95f6f4d4aaf196d67107c24e582cf2a63cb61b55
yunmingkai <asanr3@163.com>
2022-01-11 20:23:30 +0800

    整合Knife4j接口文档

--- e-commerce-alibaba-nacos-client/src/main/java/com/asan/ecommerce/controller/NacosClientController.java
+++ e-commerce-alibaba-nacos-client/src/main/java/com/asan/ecommerce/controller/NacosClientController.java
@@ -1,6 +1,10 @@
 package com.asan.ecommerce.controller;
 
 import com.asan.ecommerce.service.NacosClientService;
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +20,7 @@ import java.util.List;
  * @author mingkai yun
  * @date 2022/1/9
  */
+@Api(value = "NacosClientController", tags = "Nacos客户端接口")
 @Slf4j
 @RestController
 @RequestMapping("/nacos-client")
@@ -27,9 +32,8 @@ public class NacosClientController {
         this.nacosClientService = nacosClientService;
     }
 
-    /**
-     * <h2>根据 service id 获取服务所有的实例信息</h2>
-     */
+    @ApiOperation(value = "根据 service id 获取服务所有的实例信息", notes = "根据 service id 获取服务所有的实例信息")
+    @ApiImplicitParams({})
     @GetMapping("/service-instance")
     public List<ServiceInstance> logNacosClientInfo(
             @RequestParam(defaultValue = "e-commerce-nacos-client") String serviceId) {
