f27386a3ac59b09bd1d05e1dd8813064ce9aad87
yunmingkai <asanr3@163.com>
2022-01-13 22:07:46 +0800

    新增网关微服务模块——动态路由配置

---
+++ e-commerce-gateway/src/main/java/com/asan/ecommerce/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.asan.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author mingkai yun
+ * @date 2022/1/13
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
