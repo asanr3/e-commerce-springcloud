5edb9b9941ea177ca0913d96c9ca08f506514a5c
yunmingkai <asanr3@163.com>
2022-01-17 20:52:55 +0800

    新增用户账户功能微服务模块

---
+++ e-commerce-service/e-commerce-account-service/src/main/java/com/asan/ecommerce/AccountApplication.java
@@ -0,0 +1,22 @@
+package com.asan.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * <h1>用户账户微服务启动入口</h1>
+ * 127.0.0.1:8003/ecommerce-account-service/doc.html
+ *
+ * @author mingkai yun
+ * @date 2022/1/17
+ */
+@EnableJpaAuditing
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
